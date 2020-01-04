3b715d27a4784b0ec4cf5538ca1bfcaa32012cab
abdul-q <29357290+abdul-q@users.noreply.github.com>
2020-01-04 11:13:21 +0100

    Update day_1.rs
    Go rust - on mobile

--- rust/day_1.rs
+++ rust/day_1.rs
@@ -6,3 +6,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 Also, replace 1000 with a parameter. Such that, the problem will solve for a given value number.
 */
+use std::collections::hashmap;
+
+fn main() {
+    //
+}
+
+fn find_multiples(given_num: f64) -> Vec::<f64> {
+    //
+    mut col_3: Vec<f64> = Vec::new();
+    mut col_5: Vec<f64> = Vec::new();
+} 
